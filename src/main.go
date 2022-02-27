9aed9a3a20d738452a10a528bedf0b5b8c342a98
Masoud Moghaddam <masoud.mghd@gmail.com>
2022-02-27 23:16:03 +0330

    Implement the basic web api and mock response

--- src/main.go
+++ src/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Response struct {
+	Days []Day
+}
+
+type Day struct {
+	Title       string
+	Description string
+	IsHoliday   bool
+	IsWeekend   bool
+	Row         int
+	Column      int
+}
+
+var Result Response
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func getMonth(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	fmt.Println(key)
+	json.NewEncoder(w).Encode(Result)
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/month/{id}", getMonth)
+
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
 
 func main() {
 	fmt.Println("Welcome here!")
+	mockResponse := []Day{
+		{Title: "1"},
+		{Title: "2"},
+	}
+	Result = Response{Days: mockResponse}
+	handleRequests()
 }
