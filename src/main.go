9d959172cb71a403272216e4ce11fe4230d0cd73
Masoud Moghaddam <masoud.mghd@gmail.com>
2022-03-01 10:56:03 +0330

    Add database reader

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/gorilla/mux"
 )
@@ -23,6 +27,16 @@ type Day struct {
 	Column      int
 }
 
+type DbEvent struct {
+	Id          int
+	Year        int
+	Month       int
+	Day         int
+	Type        int
+	IsHoliday   bool
+	Description string
+}
+
 var Result Response
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -31,18 +45,77 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMonth(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Result)
+}
+
+func getMonthById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
 	fmt.Println(key)
 	json.NewEncoder(w).Encode(Result)
 }
 
+func getMonthByYearAndId(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	year, _ := strconv.Atoi(vars["year"])
+	month, _ := strconv.Atoi(vars["month"])
+	events := readFromDb(year, month)
+	json.NewEncoder(w).Encode(events)
+}
+
+func readFromDb(inputYear int, inputMonth int) []DbEvent {
+	db, err := sql.Open("sqlite3", "database/db.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query(fmt.Sprintf("SELECT * FROM Holiday WHERE Year = %d AND Month = %d", inputYear, inputMonth))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	entities := make([]DbEvent, 0)
+
+	for rows.Next() {
+		entity := DbEvent{}
+		err = rows.Scan(&entity.Id,
+			&entity.Year,
+			&entity.Month,
+			&entity.Day,
+			&entity.Type,
+			&entity.IsHoliday,
+			&entity.Description)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		entities = append(entities, entity)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return entities
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/month/{id}", getMonth)
-
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), myRouter))
+	myRouter.HandleFunc("/month", getMonth)
+	myRouter.HandleFunc("/month/{id}", getMonthById)
+	myRouter.HandleFunc("/{year}/{month}", getMonthByYearAndId)
+	port, res := os.LookupEnv("PORT")
+	if !res {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, myRouter))
 }
 
 func main() {
